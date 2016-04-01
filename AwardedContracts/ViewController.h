e552786e016ccafbe85a5f7143a37c490e407fc2
gregmartins <glauberguitars@gmail.com>
2016-04-01 11:14:03 -0700

    Initial Commit

---
+++ AwardedContracts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AwardedContracts
+//
+//  Created by Glauber Martins on 2016-04-01.
+//  Copyright © 2016 CL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AwardedContracts/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AwardedContracts
+//
+//  Created by Glauber Martins on 2016-04-01.
+//  Copyright © 2016 CL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
