9a23d6de57296ccd92dc2f4377e77d878a306766
gregmartins <glauberguitars@gmail.com>
2016-04-01 13:10:58 -0700

    Development Report:
    - An arrays is being created storing all of the elements containing in the CSV file. Structured by 7 elements in each array object
    - It is logging out the array in order to debug

--- AwardedContracts/ViewController.m
+++ AwardedContracts/ViewController.m
@@ -14,7 +14,8 @@
 
 @implementation ViewController {
     NSURL *url;
-    
+    NSString *auxData;
+    NSMutableArray *structuredData;
 }
 
 - (void)viewDidLoad {
@@ -29,13 +30,87 @@
     // Dispose of any resources that can be recreated.
 }
 
+//FETCHING THE CSV DATA FROM THE URL
 - (IBAction)btnFetch:(id)sender {
     
-    //SAVING CSV DATA IN A STRING
+    //SAVING CSV DATA INTO A STRING
     NSError *error;
     NSString *allData = [NSString stringWithContentsOfURL:url encoding:NSASCIIStringEncoding error:&error];
     NSString *noReturnData = [allData stringByReplacingOccurrencesOfString:@"\r" withString:@""];
-    NSLog(@"%@", noReturnData);
+    //NSLog(@"%@", noReturnData);
+    
+    auxData = noReturnData;
+    
+    int headerCounter = 0;
+    
+    NSMutableArray *auxArray = [[NSMutableArray alloc] init];
+    structuredData = [[NSMutableArray alloc] init];
+    
+    while ([auxData length] > 0) {
+        auxArray = [self getElements];
+        [structuredData addObject:auxArray];
+    }
+    NSLog(@"%@", structuredData);
+    
     
 }
+
+//GETTING ELEMENT BY ELEMENT BUILDING AN ARRAY OF 7 ELEMENTS
+- (NSMutableArray *) getElements {
+    
+    NSInteger elementsCounter = 0;
+    NSMutableArray *elementsArray = [[NSMutableArray alloc] init];
+    
+    NSInteger nextBreak;
+    NSString *tempElement = @"";
+    
+    while (elementsCounter < 7) {
+        //NSLog(@"%@ #####", auxData);
+        
+        //CHECKING THE EXISTENCE OF QUOTES WITHIN ELEMENTS
+        NSRange findQuote = [auxData rangeOfString:@"\""];
+        if (findQuote.location == 0) {
+            
+            auxData =[auxData substringFromIndex:1];
+            findQuote = [auxData rangeOfString:@"\""];
+            tempElement = [auxData substringToIndex:findQuote.location];
+            [elementsArray addObject:tempElement];
+            auxData = [auxData substringFromIndex:2+findQuote.location];
+            elementsCounter++;
+            continue;
+        }
+        
+        NSRange findNewLine = [auxData rangeOfString:@"\n"];
+        
+        nextBreak = -1;
+        
+        if (findNewLine.location != NSNotFound) {
+            nextBreak = findNewLine.location;
+        }
+        
+        NSRange findComma = [auxData rangeOfString:@","];
+        if (findComma.location != NSNotFound) {
+            
+            if (findComma.location < nextBreak) {
+                
+                nextBreak = findComma.location;
+                
+            }
+        }
+        
+        if (nextBreak >=0) {
+            
+            tempElement = [auxData substringToIndex:nextBreak];
+            [elementsArray addObject:tempElement];
+            auxData = [auxData substringFromIndex:1+nextBreak];
+            elementsCounter++;
+            continue;
+        }
+        
+        break;
+    }
+    
+    return elementsArray;
+}
+
 @end
